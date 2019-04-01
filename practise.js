45a9ad60c581c05495ec794f9ededb3661cbfd15
DJ <dharinij.it@gmail.com>
2019-03-31 23:01:38 -0400

    Update practise.js

--- practise.js
+++ practise.js
@@ -1,2 +1,6 @@
 
 count occurances in array - https://codesandbox.io/s/oo0rnrk6x9
+deep flatten array - https://codesandbox.io/s/q9wnqoxy9w
+difference between two arrays - https://codesandbox.io/s/4w957mopj4
+dig(Returns the target value in a nested JSON object, based on the given key.) - https://codesandbox.io/s/r7k38pp
+Replaces the names of multiple object keys with the values provided.- https://codesandbox.io/s/9jj246vzvw
