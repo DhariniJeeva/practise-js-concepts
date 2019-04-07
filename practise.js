38c842c0fce613cfb88ea4a481738c7c03f84e7d
DJ <dharinij.it@gmail.com>
2019-04-06 23:26:15 -0400

    Update practise.js

--- practise.js
+++ practise.js
@@ -4,3 +4,4 @@ deep flatten array - https://codesandbox.io/s/q9wnqoxy9w
 difference between two arrays - https://codesandbox.io/s/4w957mopj4
 dig(Returns the target value in a nested JSON object, based on the given key.) - https://codesandbox.io/s/r7k38pp
 Replaces the names of multiple object keys with the values provided.- https://codesandbox.io/s/9jj246vzvw
+array chunking - https://codesandbox.io/s/zl0z4k6r7x
